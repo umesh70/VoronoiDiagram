f532d009334af61eaa8dceeb4267bd321aec5a66
umesh <work.umesh12@gmail.com>
2024-04-17 22:45:44 +0530

    "seed generated"

--- main.c
+++ main.c
@@ -9,19 +9,26 @@
 #include<string.h>
 #include<assert.h>
 
-#define output_file_path "output.ppm"
+#define output_file_path "seeds.ppm"
 //size or resolution of the output image
 #define WIDTH 800
-#define HIEGHT 600
+#define HEIGHT 600
 #define SEED_COUNT 10
+#define SEED_RADIUS 5
+#define BACKGROUND_COLOR 0xFFFFFF
+#define SEED_COLOR 0x31363F
 
+
+typedef struct {
+    int x,y;
+}Point;
 
 //static memory for holding image
-static uint32_t image[HIEGHT][WIDTH];
-
+static uint32_t image[HEIGHT][WIDTH];
+static Point seeds[SEED_COUNT];
 void fill_image(uint32_t color){
 
-    for(size_t y = 0 ;y<HIEGHT;++y){
+    for(size_t y = 0 ;y<HEIGHT;++y){
         for(size_t x = 0; x<WIDTH;++x){
             image[y][x]=color;
         }
@@ -37,10 +44,18 @@ void SeedRender(int cx, int cy, int radius, uint32_t color){
     int y0 = cy - radius;
     int y1 = cy + radius;
 
-    for(int x = x0 ; x<x1; ++x ){
-        for(int y = y0 ; y<1; ++y){
-            
 
+    for(int x = x0 ; x<=x1; ++x ){
+        if ( 0 <= x && x<WIDTH){
+            for(int y = y0 ; y<=y1; ++y){
+               if( 0 <= y && y<HEIGHT){
+                    int dx = cx-x;
+                    int dy = cy-y;
+                    if (dx*dx + dy*dy <= radius*radius){
+                        image[y][x] = color;
+                    }
+                }
+            }
         }
     }
 }
@@ -54,16 +69,16 @@ void ppm_image(const char *filepath){
         exit(1);
     }
     //header of ppm image
-    fprintf(f,"P6\n%d %d 255\n",WIDTH,HIEGHT);
+    fprintf(f,"P6\n%d %d 255\n",WIDTH,HEIGHT);
 
-    for (size_t y = 0 ; y<HIEGHT;++y){
+    for (size_t y = 0 ; y<HEIGHT;++y){
         for (size_t x= 0 ;x<WIDTH;++x){
             //extract RGB from a pixel at y,x position
             uint32_t pixel = image[y][x];
             uint8_t bytes[3] = {
                 (pixel&0x0000FF)>>8*0,
                 (pixel&0x00FF00)>>8*1,
-                (pixel&0x0FF000)>>8*2,
+                (pixel&0xFF0000)>>8*2,
             };
             fwrite(bytes,sizeof(bytes),1,f);
             assert(!ferror(f));
@@ -74,9 +89,19 @@ void ppm_image(const char *filepath){
     assert(ret == 0);
 }
 
+void generate_Seeds(){
+    for (size_t i = 0 ;i<SEED_COUNT;++i){
+        seeds[i].x = rand()%WIDTH;
+        seeds[i].y = rand()%HEIGHT;
+    }
+}
 int main(void){
-
-    fill_image(0xFF0000FF);
+    fill_image(BACKGROUND_COLOR);
+    generate_Seeds();
+    for (size_t i = 0 ;i<SEED_COUNT;++i){
+        SeedRender(seeds[i].x,seeds[i].y,SEED_RADIUS,SEED_COLOR);
+    }
+    
     ppm_image(output_file_path);
     return 0;
 }
